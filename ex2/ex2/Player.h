37cfba6495e1d8c62bb09da0953a80280b70f4ab
ofirgott <gottesmanofir@gmail.com>
2017-04-24 01:43:07 +0300

    changs player members from private to protected

--- ex2/ex2/Player.h
+++ ex2/ex2/Player.h
@@ -13,7 +13,7 @@ public:
 	bool init(const std::string& path) override { return id != -1; }
 	void setBoard(int player, const char** board, int numRows, int numCols) override;
 
-private:
+protected:
 	int id;				// players id , if id == -1 player is invalid 
 	int tScore;			// how much score the player gained this far
 	Ship***  pBoard;	// matrix of pointers to ships  
