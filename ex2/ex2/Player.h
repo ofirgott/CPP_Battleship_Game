2b6dafde4262176ff4b6d138096364e62f68ad69
ofirgott <gottesmanofir@gmail.com>
2017-04-25 19:31:15 +0300

    update player cto'r

--- ex2/ex2/Player.h
+++ ex2/ex2/Player.h
@@ -9,13 +9,13 @@ class Player : public IBattleshipGameAlgo
 	static std::vector<IBattleshipGameAlgo *> _instancesVec; //our player collection
 
 public:
-	Player(int playerId = -1) : id(playerId) {};//todo: enter the other members to the init list!!!!!!!!!
+	Player() : id(-1), tScore(0), cShips(0) {};					//todo: we need to give the player his ID in the init function, and not in the cto'r
 	virtual ~Player();
 	bool init(const std::string& path) override { return id != -1; }
 	void setBoard(int player, const char** board, int numRows, int numCols) override;
 
 protected:
-	int id;				// players id , if id == -1 player is invalid 
+	int id;				// players id , if id == -1 player is invalid or undefined
 	int tScore;			// how much score the player gained this far
 	Ship***  pBoard;	// matrix of pointers to ships  
 	int cShips;			// number of ships player has
