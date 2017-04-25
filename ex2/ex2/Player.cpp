169ae7c033ae3c2e6243ec7d8bd76c5df0181f8b
ofirgott <gottesmanofir@gmail.com>
2017-04-25 20:35:29 +0300

    update player dto'r

--- ex2/ex2/Player.cpp
+++ ex2/ex2/Player.cpp
@@ -7,13 +7,13 @@ Player::~Player()
 {
 	for (auto it = _instancesVec.begin(); it != _instancesVec.end(); ++it)
 	{
-		Ship::deleteShipMatrix(it->);
 		delete(*it);
 	}
 }
 
 void Player::setBoard(int player, const char** board, int numRows, int numCols)
 {
+	
 	// copy board
 	auto copyBoard = BattleshipBoard::copyMatrix(const_cast<char**>(board), numRows, numCols);		//copyBoard is char**
 
