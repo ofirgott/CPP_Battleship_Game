8dc15a90f976209a52c78b8fcc02550211405e1c
ofirgott <gottesmanofir@gmail.com>
2017-04-25 21:14:49 +0300

    update player getters

--- ex2/ex2/Player.h
+++ ex2/ex2/Player.h
@@ -11,8 +11,28 @@ class Player : public IBattleshipGameAlgo
 public:
 	Player() : id(-1), tScore(0), cShips(0) {};					//todo: we need to give the player his ID in the init function, and not in the cto'r
 	virtual ~Player();
+	Player& operator=(const Player& otherPlayer) = delete;			/* deletes the assignment operator - we want a player to be a Non Copyable object */
+
 	bool init(const std::string& path) override { return id != -1; }
 	void setBoard(int player, const char** board, int numRows, int numCols) override;
+
+	/* @return total score gained by player
+	*/
+	int getTScore()const { return tScore;  }
+
+	/* @ return how much ships player has
+	*/
+	int getShipsCount()const { return cShips; }
+
+	/*@ return - players id
+	*/
+	int getID()const { return id; }
+
+	/* update players score to the prev score + num
+	* @assume- num >= 0
+	*/
+	void updateScore(int points) { if(points > 0) tScore += points; }
+
 
 protected:
 	int id;				// players id , if id == -1 player is invalid or undefined
--- ex2/ex2/PlayerFromFile.h
+++ ex2/ex2/PlayerFromFile.h
@@ -1,10 +1,17 @@
 #pragma once
 #include "Player.h"
+#include "Ship.h"
 
 class PlayerFromFile : public Player
 {
 public:
 	PlayerFromFile() : Player() {};
+	~PlayerFromFile() { Ship::deleteShipMatrix(pBoard);  }
+
+	std::pair<int, int> attack() override;
+
+	void notifyOnAttackResult(int player, int row, int col, AttackResult result) = 0;	// notify on last move result
+
 
 
 };
