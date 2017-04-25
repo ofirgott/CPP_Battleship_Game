c69d96798c445c2b49255e6433102f41040de526
ofirgott <gottesmanofir@gmail.com>
2017-04-25 19:43:24 +0300

    update types to auto where we can in player

--- ex2/ex2/Player.cpp
+++ ex2/ex2/Player.cpp
@@ -14,19 +14,18 @@ Player::~Player()
 void Player::setBoard(int player, const char** board, int numRows, int numCols)
 {
 	// copy board
-	char** copyBoard = BattleshipBoard::copyMatrix(const_cast<char**>(board), numRows, numCols);
+	auto copyBoard = BattleshipBoard::copyMatrix(const_cast<char**>(board), numRows, numCols);		//copyBoard is char**
 
 	// create a set containing all ships details i.e letter and coordinated for each ship (extract from board)
-	std::set<std::pair<char, std::set<std::pair<int, int>>>> allShipsDetails = BattleshipBoard::ExtractShipsDetails(copyBoard, numRows, numCols);
+	auto allShipsDetails = BattleshipBoard::ExtractShipsDetails(copyBoard, numRows, numCols);		// allShipsDetails is std::set<std::pair<char, std::set<std::pair<int, int>>>>
 
 	// free memory allocated for copyBoard
 	BattleshipBoard::deleteMatrix(copyBoard, numRows, numCols);
 
 	// for each ship detail in allShipsDetails allocate a new ship ith this details
-	std::set<Ship*> shipsSet = Ship::createShipSet(allShipsDetails);
+	auto shipsSet = Ship::createShipSet(allShipsDetails);		//shipsSet is std::set<Ship*>
 
 	// create matrix of pointers to the ships allocated for this player
 	pBoard = Ship::createShipMatrix(shipsSet);
-
 	
 }
