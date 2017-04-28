95f1e7caa4f41356d73622f44ddecf06eb784fa3
ofirgott <gottesmanofir@gmail.com>
2017-04-28 16:50:23 +0300

    insert iterator to PlayerFromFile.h

--- ex2/ex2/PlayerFromFile.h
+++ ex2/ex2/PlayerFromFile.h
@@ -5,7 +5,7 @@
 class PlayerFromFile : public Player
 {
 public:
-	PlayerFromFile() : Player(), cAttack(0), isDone(false) {};	//todo: i want to use iterator instead
+	PlayerFromFile() : Player(), isDone(false) {};	//todo: i want to use iterator instead
 
 	std::pair<int, int> attack() override;
 
@@ -15,9 +15,9 @@ public:
 
 
 private:
-	int cAttack; // index of the next attack in pMoves		todo: maybe we want an iterator instead
-	std::vector<std::pair<int, int>> pMoves; // vector of moves
-	bool isDone; // if false player has more moves; true - no more moves	todo: maybe we want an iterator instead
+	std::vector<std::pair<int, int>> movesVector; // vector of moves
+	std::vector<std::pair<int, int>>::const_iterator movesVectorItr;
+	bool isDone; // if false player has more moves; true - no more moves	
 
 
 };
