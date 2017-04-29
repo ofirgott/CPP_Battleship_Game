93d88b8b7c0e7d190d1937122995f0312a3e3c93
ofirgott <gottesmanofir@gmail.com>
2017-04-29 11:14:23 +0300

    done with init in PlayerFromFile

--- ex2/ex2/PlayerFromFile.cpp
+++ ex2/ex2/PlayerFromFile.cpp
@@ -3,19 +3,35 @@
 
 std::pair<int, int> PlayerFromFile::attack()
 {
-	return std::pair<int, int>();
+	return std::pair<int, int>();		//todo: implement
 }
 
 bool PlayerFromFile::init(const std::string & path)
 {
-	if (id == UNDEFINED_PLAYERID)
+	if (id == UNDEFINED_PLAYERID || !BattleshipGameUtils::isValidDir(path))
 		return false;
 
-	if (!BattleshipGameUtils::isValidDir(path))
-		return false;
-
+	int attackFileOrder;
+	auto attackFilenames = BattleshipGameUtils::SortedDirlistSpecificExtension(path, ".attack");
 	
+	if(attackFilenames.size() == 0)			/* no relevant attack files in path */
+	
+		return false;
+	
+	else if (attackFilenames.size() == 1)
+		attackFileOrder = 0;
+	
+	else
+	{
+		if (id == PLAYERID_A) attackFileOrder = 0;
+		else attackFileOrder = 1;
+	}
 
+	auto myAttackFilePath = path + "\\" + attackFilenames[attackFileOrder];
+	
+	movesVector = parseAttackFile(myAttackFilePath);	//todo : add parseAttackFile to the code 
+
+	movesVectorItr = movesVector.begin();
 
 	return true;
 }
