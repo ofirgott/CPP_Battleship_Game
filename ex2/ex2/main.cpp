a4f47a2b6fa48c892eff66893df2747a41eb30d9
ofirgott <gottesmanofir@gmail.com>
2017-04-30 15:32:47 +0300

    update main file

--- ex2/ex2/main.cpp
+++ ex2/ex2/main.cpp
@@ -3,6 +3,21 @@
 
 int main(int argc, char* argv[])
 {
-	
+	bool gameSuccessfullyCreated = false;							/* boolean for the game constructor */
+
+	try
+	{
+		BattleshipGameManager Game(argc, argv, gameSuccessfullyCreated);
+		if (gameSuccessfullyCreated)
+			Game.Run();
+		else
+			return -1;
+	}
+	catch (std::exception ex)
+	{
+		return -1;
+	}
+
+	return 0;
 
 }
