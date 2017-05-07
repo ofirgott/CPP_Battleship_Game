17e8906d6a49b9ffbd2bf91652ae82dac4be5627
sharon2108 <sharon2108@gmail.com>
2017-05-07 16:20:36 +0300

    destrucor in naive

--- ex2/ex2/PlayerNaive.h
+++ ex2/ex2/PlayerNaive.h
@@ -5,10 +5,14 @@
 class PlayerNaive : public Player
 {
 public:
+	PlayerNaive() : Player() {};
+	~PlayerNaive() = default;
+
 	std::pair<int, int> attack() override;
 	bool init(const std::string& path) override;
 	void notifyOnAttackResult(int player, int row, int col, AttackResult result) override {} //empty implementaion
 	void setBoard(int player, const char** board, int numRows, int numCols) override;
+	
 private:
 	std::vector<std::pair<int, int>> movesVector; // vector of moves
 	std::vector<std::pair<int, int>>::const_iterator movesVectorItr;
