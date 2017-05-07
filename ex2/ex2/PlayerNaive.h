411db6e0b49411a02923bdb3b9ebad84caa47432
sharon2108 <sharon2108@gmail.com>
2017-05-07 09:19:52 +0300

    update player naive

--- ex2/ex2/PlayerNaive.h
+++ ex2/ex2/PlayerNaive.h
@@ -1,1 +1,17 @@
 #pragma once
+#include "Player.h"
+
+class PlayerNaive : public Player
+{
+public:
+	std::pair<int, int> attack() override;
+	bool init(const std::string& path) override;
+	void notifyOnAttackResult(int player, int row, int col, AttackResult result) override {} //empty implementaion
+	void setBoard(int player, const char** board, int numRows, int numCols) override;
+private:
+	std::vector<std::pair<int, int>> movesVector; // vector of moves
+	std::vector<std::pair<int, int>>::const_iterator movesVectorItr;
+	bool isDone; // if false player has more moves; true - no more moves	
+
+
+};
