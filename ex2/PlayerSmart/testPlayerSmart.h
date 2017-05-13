0db839ab59749366a3b5f12514d85adbccd556a7
sharon2108 <sharon2108@gmail.com>
2017-05-13 17:46:41 +0300

    start of test playersmart

---
+++ ex2/PlayerSmart/testPlayerSmart.h
@@ -0,0 +1,45 @@
+#pragma once
+#include"PlayerSmart.h"
+#include  <iostream>;
+
+class testShipInProcess {
+public:
+	testShipInProcess() {};
+	~testShipInProcess() {};
+
+	static int testremoveSankFromReleventCoors() {
+	}
+	static int testnotifyOnAttackResult() {
+	}
+	//removeOneCoordinate
+	static int testremoveOneCoordinate() {
+	}
+	//removeAllIrreleventCoordinates
+	static int testremoveAllIrreleventCoordinates() {
+	}
+	//isInAttackOptions
+	static int testisInAttackOptions() {
+	}
+	//findPairInAttackedShips
+	static int testfindPairInAttackedShips() {
+	}
+	//mergeShipDetails
+	static int testmergeShipDetails() {
+	}
+	//addCoorToShipsInProcess
+	static int testaddCoorToShipsInProcess() {
+	}
+	//nextAttackFromCoors
+	static int testnextAttackFromCoors() {
+	}
+	//sizeOneAttack
+	static int testsizeOneAttack() {
+	}
+	//attack
+	static int testattack() {
+	}
+	//setBoard
+	static int testsetBoard() {
+	}
+
+};
