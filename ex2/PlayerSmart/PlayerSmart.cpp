532277e1cfcc2119b655c218a041f50ec8f43186
dianakolton <danunak2@gmail.com>
2017-05-14 16:46:12 +0300

    ...

--- ex2/PlayerSmart/PlayerSmart.cpp
+++ ex2/PlayerSmart/PlayerSmart.cpp
@@ -385,7 +385,6 @@ void PlayerSmart::removeSankFromReleventCoors(int indexOfPair)
 }
 
 
-
 IBattleshipGameAlgo* GetAlgorithm()
 {
 	_instancesVec.push_back(new PlayerSmart());  // Create new instance and keep it in vector
--- ex2/PlayerSmart/PlayerSmart.h
+++ ex2/PlayerSmart/PlayerSmart.h
@@ -8,7 +8,6 @@
 
 class PlayerSmart : public Player
 {
-	friend class testPlayerSmart;//////////////////////////////////////////////////////////////delete!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
 public:
 
