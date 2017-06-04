927e2dcd2fe4f4a1892b9ca697ae73191d1ca9c4
ofirgott <gottesmanofir@gmail.com>
2017-06-04 19:54:31 +0300

    end with standingsVector

--- ex3/ex3/BattleshipPrint.cpp
+++ ex3/ex3/BattleshipPrint.cpp
@@ -1,55 +1,6 @@
-#pragma once
-
-#include "BattleshipBoard.h"
-#include <windows.h>
-#include "IBattleshipGameAlgo.h"
-#include "StandingsTableEntryData.h"
+#include "BattleshipPrint.h"
 #include <iostream>
 #include <iomanip>
-
-
-class BattleshipPrint
-{
-public:
-
-	static const int RED_COLOR = 12;
-	static const int BLUE_COLOR = 9;
-	static const int YELLOW_COLOR = 14;
-	static const int WHITE_COLOR = 15;
-	static const int PLAYER_A_COLOR = BLUE_COLOR;
-	static const int PLAYER_B_COLOR = YELLOW_COLOR;
-	static const int ATTACK_COLOR = RED_COLOR;
-	static const char ATTACK_CHAR = '@';
-	static const char HIT_CHAR = 'X';
-	static const char BLANK_CHAR = ' ';
-
-	const static int printDefatultDealy = 300;
-	const static bool printDeafultIsQuiet = false;
-
-	static void printStartBoard(const BattleshipBoard& board);
-	static void printCurrentAttack(int playerAttackedId, std::pair<int, int> coord, char currChar, AttackResult res);
-
-	static void setDelay(int newDelay) { delay = newDelay; }
-	static void setIsQuiet(bool newIsQuiet) { isQuiet = newIsQuiet; }
-	static bool IsQuiet() { return isQuiet; }
-	static int getDelay() { return delay; }
-	static void setCursorAfterBoard(int rows) { if (!isQuiet) { gotoxy(0, rows + 5); showCursor(); } }
-
-
-	/* new functions */
-	static void printStandingsTable(std::vector<StandingsTableEntryData> sortedPlayersStandings);
-	//todo: we need for this function: wins, loses, ptsFor, ptsAgainst. and also we need that vector to be sorted	
-private:
-	BattleshipPrint() = delete;
-	static bool isQuiet;
-	static int delay;
-
-	static void gotoxy(const int x, const int y);
-	static void hideCursor();
-	static void showCursor();
-	static void setColor(WORD color);
-
-};
 
 void BattleshipPrint::printStandingsTable(std::vector<StandingsTableEntryData> sortedPlayersStandings)
 {
@@ -58,7 +9,7 @@ void BattleshipPrint::printStandingsTable(std::vector<StandingsTableEntryData> s
 	/* Table headers */
 	std::cout << "#\tTeam Name\t\tWins\tLosses\t%\tPts For\tPts Against\n\n";
 
-	for(auto playerData: sortedPlayersStandings)
+	for (auto playerData : sortedPlayersStandings)
 	{
 		std::cout << placeNum << ".\t";
 		std::cout << playerData.playerName << "\t\t";
@@ -72,5 +23,5 @@ void BattleshipPrint::printStandingsTable(std::vector<StandingsTableEntryData> s
 	}
 
 	std::cout << std::endl;
-	
+
 }
--- ex3/ex3/StandingsTableEntryData.h
+++ ex3/ex3/StandingsTableEntryData.h
@@ -1,12 +1,47 @@
 #pragma once
 #include <string>
+#include <algorithm>
 
-
-struct StandingsTableEntryData
+/* we use class and not struct because we want to hide this information*/
+class StandingsTableEntryData
 {
+public:
+
+	StandingsTableEntryData() : playerName(""), winsCnt(0), lossesCnt(0), pointsFor(0), pointsAgainst(0) {}
+	StandingsTableEntryData(std::string name, int wins, int losses, int pFor, int pAgainst) : playerName(name), winsCnt(wins), lossesCnt(losses), pointsFor(pFor), pointsAgainst(pAgainst) {}
+
+	~StandingsTableEntryData() = default;
+	StandingsTableEntryData(const StandingsTableEntryData& other) = delete;					/* deletes copy constructor */
+
+	/* getters */
+	std::string PlayerName()const { return playerName; }
+	int WinsNumber()const { return winsCnt; }
+	int LossesNumber()const { return lossesCnt; }
+	int PointsFor()const { return pointsFor; }
+	int PointsAgainst()const { return pointsAgainst; }
+
+	/* setters*/
+	void setWinsNumber(int newWinsNum) {  winsCnt = newWinsNum; }
+	void setLossesNumber(int newLossesNum) {  lossesCnt = newLossesNum; }
+	void setPointsFor(int newPointsForNum) {  pointsFor = newPointsForNum; }
+	void setPointsAgainst(int newPointsAgainstNum) {  pointsAgainst = newPointsAgainstNum; }
+
+
+	/* assume that we compare players with the same number of games */
+	bool operator > (const StandingsTableEntryData& other)const { 
+		if (winsCnt != other.winsCnt)
+			return winsCnt > other.winsCnt;
+		else
+			return pointsFor > other.pointsFor;
+	}
+	
+	static void sortPlayersStandingsVector(std::vector<StandingsTableEntryData> standingsVector) { std::sort(standingsVector.begin(), standingsVector.end()); }
+
+private:
+
 	std::string playerName;
 	int winsCnt;
 	int lossesCnt;
-	int pointsForCnt;
-	int pointsAgainstCnt;
+	int pointsFor;
+	int pointsAgainst;
 };
