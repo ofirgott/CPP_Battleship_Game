4c5f437ec8bdec4b7b6c1c3a9d85076d97f81120
ofirgott <gottesmanofir@gmail.com>
2017-06-07 01:34:42 +0300

    update single game manager - delete function that already in the tournament manager

--- ex3/ex3/BattleshipGameManager.cpp
+++ ex3/ex3/BattleshipGameManager.cpp
@@ -1,42 +1,10 @@
 #include "BattleshipGameManager.h"
 #include "BattleshipGameUtils.h"
 #include "GamePlayerData.h"
-#include "PrintGameBoard.h"
 
 
-BattleshipGameManager::BattleshipGameManager(int argc, char * argv[]) : playerAlgoA(nullptr), playerAlgoB(nullptr), inputDirPath(""), gameSuccessfullyCreated(false)
-{
-	std::string dllPathPlayerA = "", dllPathPlayerB = "", boardPath = "";
 
-	if (!checkGameArguments(argc, argv)) return;									/* parse the */
 
-	if (!checkGamefiles(boardPath, dllPathPlayerA, dllPathPlayerB)) return;
-
-	mainBoard = BattleshipBoard(boardFilePath, DEAFULT_BOARD_ROWS, DEAFULT_BOARD_COLS);
-
-	if (!checkMainBoardValidity() || !initGamePlayers(dllPathPlayerA, dllPathPlayerB)) return;
-
-	gameSuccessfullyCreated = true;
-
-	PrintGameBoard::printStartBoard(mainBoard);
-
-}
-
-BattleshipGameManager::~BattleshipGameManager()
-{
-
-	delete playerAlgoA;
-	delete playerAlgoB;
-
-	std::vector<std::pair<int, HINSTANCE>>::iterator vitr;
-
-	// close all the dynamic libs we opened 
-
-	for (vitr = dll_vec.begin(); vitr != dll_vec.end(); ++vitr)
-	{
-		FreeLibrary(vitr->second);
-	}
-}
 
 void BattleshipGameManager::Run()
 {
@@ -116,7 +84,7 @@ void BattleshipGameManager::Run()
 	}
 
 	// prints game results 
-	PrintGameBoard::setCursorAfterBoard(mainBoard.getRows());
+	
 	outputGameResult(currPlayer, otherPlayer);
 
 	return;
@@ -162,199 +130,6 @@ void BattleshipGameManager::outputGameResult(GamePlayerData* currPlayer, GamePla
 	}
 }
 
-bool BattleshipGameManager::checkGameArguments(int argc, char *argv[])
-{
-	std::string path = ".";
-	bool isQuiet = PrintGameBoard::printDeafultIsQuiet;
-	int printDelay = PrintGameBoard::printDefatultDealy;
-
-	if (argc > 5)
-	{
-		std::cout << "Error, Too many arguments!" << std::endl;
-		return false;
-	}
-
-	for (int i = 1; i < argc; i++)
-	{
-		if (strcmp(argv[i], "-quiet") == 0)
-		{
-			isQuiet = true;
-		}
-		else if (strcmp(argv[i], "-delay") == 0)
-		{
-			if (i == argc - 1) {	/* we got delay flag but this is the last argument - this is an error because we need the int*/
-				std::cout << "Error, got -delay flag, but delay value is missing." << std::endl;
-				return false;
-			}
-			else
-			{
-				char* stringEnd = nullptr;
-				printDelay = (int)strtol(argv[++i], &stringEnd, 10);
-				if (*stringEnd)
-				{
-					std::cout << "Error, -delay value is not a valid integer" << std::endl;
-					return false;
-				}
-			}
-		}
-		else  /* this is the dir path */
-		{
-			path = argv[i];
-		}
-
-	}
-
-	if (!BattleshipGameUtils::getFullPath(path)) return false;
-
-	inputDirPath = path;
-
-	PrintGameBoard::setIsQuiet(isQuiet);
-	PrintGameBoard::setDelay(printDelay);
-
-	return true;
-}
-
-bool BattleshipGameManager::checkGamefiles(std::string & boardPath, std::string & dllPathPlayerA, std::string & dllPathPlayerB)
-{
-
-
-	if (!BattleshipGameUtils::isValidDir(inputDirPath)) {									/* checks if directory in dir_path exists */
-		std::cout << "Wrong path: " << inputDirPath << std::endl;
-		return false;
-	}
-	/* finds the board file */
-	auto tmpFilenamesVector = BattleshipGameUtils::SortedDirlistSpecificExtension(inputDirPath, ".sboard");
-
-	if (tmpFilenamesVector.size() == 0) {
-		std::cout << "Missing board file (*.sboard) looking in path: " << inputDirPath << std::endl;
-		return false;
-	}
-	else boardFilePath = inputDirPath + "/" + tmpFilenamesVector[0];
-
-	tmpFilenamesVector = BattleshipGameUtils::SortedDirlistSpecificExtension(inputDirPath, ".dll");
-
-	if (tmpFilenamesVector.size() < 2) {
-		std::cout << "Missing an algorithm (dll) file looking in path: " << inputDirPath << std::endl;
-		return false;
-	}
-
-	dllPathPlayerA = inputDirPath + "/" + tmpFilenamesVector[0];
-	dllPathPlayerB = inputDirPath + "/" + tmpFilenamesVector[1];
-
-
-
-
-	return true;
-}
-
-
-
-bool BattleshipGameManager::checkMainBoardValidity()const
-{
-	if (!mainBoard.isSuccessfullyCreated()) return false;
-
-	bool hasShipWithWrongSize_A, hasShipWithWrongSize_B, hasTooManyShip_A, hasToManyShip_B;
-	bool hasTooFewShips_A, hasTooFewShips_B, hasAdjacentShips;
-
-	size_t validShipsCnt_A, validShipsCnt_B;
-	hasShipWithWrongSize_A = hasShipWithWrongSize_B = hasTooManyShip_A = hasToManyShip_B = hasAdjacentShips = false;
-	hasTooFewShips_A = hasTooFewShips_B = true;
-
-	const char** matrixA = const_cast<const char**>(mainBoard.createPlayerBoard(PLAYERID_A));						/* allocates new matrix */
-	BattleshipBoard boardA(matrixA, mainBoard.getRows(), mainBoard.getCols());
-	const char** MatrixB = const_cast<const char**>(mainBoard.createPlayerBoard(PLAYERID_B));
-	BattleshipBoard boardB(MatrixB, mainBoard.getRows(), mainBoard.getCols());
-	std::pair<size_t, std::set<char>> tmpPair;											/* for FindNumberOfValidShipsInBoard output*/
-	std::set<char> invalidShips_A;
-	std::set<char> invalidShips_B;
-
-
-	tmpPair = FindNumberOfValidShipsInBoard(boardA);
-	validShipsCnt_A = tmpPair.first;
-	invalidShips_A = tmpPair.second;
-	BattleshipBoard::deleteMatrix(matrixA, boardA.getRows(), boardA.getCols());
-
-	tmpPair = FindNumberOfValidShipsInBoard(boardB);
-	validShipsCnt_B = tmpPair.first;
-	invalidShips_B = tmpPair.second;
-	BattleshipBoard::deleteMatrix(MatrixB, boardB.getRows(), boardB.getCols());
-
-	PrintWrongSizeOrShapeForShips(invalidShips_A, A);
-	PrintWrongSizeOrShapeForShips(invalidShips_B, B);
-
-	bool isCorrectShipsNumA = isCorrectNumberOfShipsForPlayer(validShipsCnt_A, A);
-	bool isCorrectShipsNumB = isCorrectNumberOfShipsForPlayer(validShipsCnt_B, B);
-
-
-	hasAdjacentShips = mainBoard.CheckIfHasAdjacentShips();								/* if has adjacent ships, this funcion also prints relevant message */
-
-	if (isCorrectShipsNumA && isCorrectShipsNumB && !hasAdjacentShips && invalidShips_A.empty() && invalidShips_B.empty())
-		return true;
-	else
-		return false;
-
-}
-
-std::pair<size_t, std::set<char>> BattleshipGameManager::FindNumberOfValidShipsInBoard(BattleshipBoard& board)
-{
-	std::set<std::pair<char, std::set<std::pair<int, int>>>> setOfShipsDetails;					/* set of ships details - for example:
-																								{<'m', {<1,2>,<1,3>}> , <'P', {<8,5>, <8,6> , <8,7>}> } */
-	std::set<char> invalidShips;																 /* set of the invalid ships (to avoid duplicated ships in error messages) */
-
-
-	setOfShipsDetails = board.ExtractShipsDetails();	         /* after this row, we have set of ships, maybe some of them invalid -  this function will 																							  be called also when we will create the player */
-
-
-	DeleteInvalidShipsDetailsEntryFromSet(setOfShipsDetails, invalidShips);						 /* after this row, we have only valid ships in setOfShipsDetails, and alse invalidShips																								  updated*/
-
-	return std::make_pair(setOfShipsDetails.size(), invalidShips);								 /* return number of valid ships and set of letters of found invalid ships */
-}
-
-void BattleshipGameManager::DeleteInvalidShipsDetailsEntryFromSet(std::set<std::pair<char, std::set<std::pair<int, int>>>>& setOfShipsDetails, std::set<char>& invalidShips)
-{
-	auto it = setOfShipsDetails.begin();
-	while (it != setOfShipsDetails.end())
-	{
-		if (!Ship::isValidShipDetails(*it))
-		{
-			invalidShips.insert(it->first);
-			it = setOfShipsDetails.erase(it);
-		}
-		else
-		{
-			++it;
-		}
-	}
-
-}
-
-void BattleshipGameManager::PrintWrongSizeOrShapeForShips(std::set<char>& invalidShipsSet, char playerChar)
-{
-	if (invalidShipsSet.empty()) return;
-
-	for (char shipChar : invalidShipsSet)
-	{
-		std::cout << "Wrong size or shape for ship " << shipChar << " for player " << playerChar << std::endl;
-	}
-}
-
-bool BattleshipGameManager::isCorrectNumberOfShipsForPlayer(size_t validShipsCnt, char playerChar)
-{
-
-	if (validShipsCnt == NUM_OF_PLAYER_SHIPS) return true;
-
-	else if (validShipsCnt < NUM_OF_PLAYER_SHIPS)
-	{
-		std::cout << "Too few ships for player " << playerChar << std::endl;
-		return false;
-	}
-
-	else 		//validShipsCnt > NUM_OF_PLAYER_SHIPS
-	{
-		std::cout << "Too many ships for player " << playerChar << std::endl;
-		return false;
-	}
-}
 
 bool BattleshipGameManager::loadAndInitPlayerDll(const std::string & dllPathPlayer, IBattleshipGameAlgo* &player, int playerId, HINSTANCE& hDll, Ship*** &shipsMatrix, size_t& shipsCnt)const
 {
--- ex3/ex3/BattleshipGameManager.h
+++ ex3/ex3/BattleshipGameManager.h
@@ -17,9 +17,9 @@ public:
 	static const int PLAYERID_B = 1;														/* default id for player B */
 	static const int UNDEFINED_PLAYERID = -1;												/* default id for undefined or invalid player */
 
-
-	BattleshipGameManager(int argc, char* argv[]);											/* constructor */
-	~BattleshipGameManager();
+	/* todo: see if gamesucc bool is nessceary*/
+	BattleshipGameManager(const BattleshipBoard& board, IBattleshipGameAlgo* algoA, IBattleshipGameAlgo* algoB) : mainBoard(board), playerAlgoA(algoA), playerAlgoB(algoB), gameSuccessfullyCreated(true) {}
+	~BattleshipGameManager() = default;
 
 	BattleshipGameManager() = delete;														/* deletes empty constructor */
 	BattleshipGameManager(const BattleshipGameManager& otherGame) = delete;					/* deletes copy constructor */
@@ -35,12 +35,6 @@ private:
 
 	static const char A = 'A';																/* player char for player A - for printing */
 	static const char B = 'B';																/* player char for player B */
-	static const int DEAFULT_BOARD_ROWS = 10;												/* default rows number for the main board */
-	static const int DEAFULT_BOARD_COLS = 10;												/* default cols number for the main board */
-	static const int NUM_OF_PLAYER_SHIPS = 5;												/* number of valid ships for each game player */
-
-
-	std::vector<std::pair<int, HINSTANCE>> dll_vec;											/* vector of <playerID, dll handle> */
 
 	GamePlayerData playerA;																	/* an object that keeps all relevant data of playerA in the game */
 	GamePlayerData playerB;																	/* an object that keeps all relevant data of playerB in the game */
@@ -48,39 +42,11 @@ private:
 	IBattleshipGameAlgo* playerAlgoA;														/* pointer to the algo of playerA - will deleted in **this** class destructor */
 	IBattleshipGameAlgo* playerAlgoB;														/* pointer to the algo of playerB - will deleted in **this** class destructor */
 
-
-
-
-
-	std::string inputDirPath;
-	std::string boardFilePath;
-
 	BattleshipBoard mainBoard;
 	bool gameSuccessfullyCreated;
 
 	static void outputGameResult(GamePlayerData* currPlayer, GamePlayerData* otherPlayer);
-	bool checkGameArguments(int argc, char* argv[]);
-	bool checkGamefiles(std::string& boardPath, std::string& dllPathPlayerA, std::string& dllPathPlayerB);
-
-	/* given a game main board, returns true if the board is valid according to the game rules:
-	right number of valid ships for each player, no invalid and adjacent ships in board. */
-	bool BattleshipGameManager::checkMainBoardValidity()const;
-
-	/* given a matrix board for a specific player, returns number of valid ships and set of invalid ships letters (according to the game rules
-	for example - <5, {'M', 'P'}> - input player board has 5 valid ships, but invalid size or shape 'M' and 'P' ships  */
-	static std::pair<size_t, std::set<char>> FindNumberOfValidShipsInBoard(BattleshipBoard& board);
-
-	/* given a set of sips details for player, this function deletes invalid ships from the set, according to the game rules */
-	/* in addition, adds letters of deleted found invalid ships to the set invalidShips*/
-	static void DeleteInvalidShipsDetailsEntryFromSet(std::set<std::pair<char, std::set<std::pair<int, int>>>>& setOfShipsDetails, std::set<char>& invalidShips);
-
-
-	/* given a player name and set of invalid ships letter for this player , prints relevant error message for each invalid ship in set */
-	static void PrintWrongSizeOrShapeForShips(std::set<char>& invalidShipsSet, char playerChar);
-
-	/* given a player name and his set of valid ships, returns true if the player has NUM_OF_PLAYER_SHIPS valid ships,
-	else prints relevant error message (for too many / too few ships), and returns false */
-	static bool isCorrectNumberOfShipsForPlayer(size_t validShipsCnt, char playerChar);
+	
 
 	bool initGamePlayers(const std::string& dllPathPlayerA, const std::string& dllPathPlayerB);
 
