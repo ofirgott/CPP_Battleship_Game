d6934f7401de339bddbdbe61a508d3fcc8f489a6
ofirgott <gottesmanofir@gmail.com>
2017-07-02 11:39:08 +0300

    FINAL!

---
+++ ex4/ex4/Submission/main.cpp
@@ -0,0 +1,37 @@
+#include "Matrix.h"
+
+
+template<typename Groups>
+void print(const Groups& all_groups) {
+	for (const auto& groupType : all_groups) {
+		std::cout << groupType.first << ":" << std::endl;
+		for (const auto& groupOfType : groupType.second) {
+			for (const auto& coord : groupOfType) {
+				std::cout << "{ ";
+				for (auto i : coord) {   // was: for(int i : coord)
+					std::cout << i << ' ';
+				}
+				std::cout << "} ";
+			}
+			std::cout << std::endl;
+		}
+	}
+}
+
+
+int main() {
+
+	std::cout << "2D expample: " << std::endl << std::endl ;
+	Matrix2d<char> m = { { 'a', 'A', 'a' },{ 'B', 'a', 'B' },{ 'B', 'a', 'B' } };
+	auto all_groups = m.groupValues([](auto i) {return islower(i) ? "L" : "U"; });
+	print(all_groups);
+
+	std::cout << std::endl << std::endl;
+
+	std::cout << "3D expample: " << std::endl << std::endl;
+	Matrix3d<int> m2 = { { { 1, 2, 3 },{ 1, 2 },{ 1, 2 } },{ { 1, 2 },{ 1, 2, 3, 4 } } };
+	auto groups = m2.groupValues([](auto i) {return i % 3 ? "!x3" : "x3"; });
+	print(groups);
+
+	return 0;
+}
